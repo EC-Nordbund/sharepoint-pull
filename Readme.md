f7e41d42e84b609907bdf9ffa0cef36649a34c28
root <root@vps18234.servercow.local>
2021-01-19 08:16:32 +0100

    delete old files

--- Readme.md
+++ Readme.md
@@ -9,3 +9,7 @@ docker build -t sharepoint-pull .
 ```sh
 docker run -v /samba-share/test-folder:/sharepoint --env SP_BASE_URL=https://who.sharepoint.com/sites/team --env SP_ROOT_FOLDER=Servertest --env SP_CRED_USER=user@domain.de --env SP_CRED_PASS=pass sharepoint-pull
 ```
+
+## Delete old files
+***IMPORTANT:*** Files that are in sharepoint and got changed will overwrite local files. If a file is deleted in Sharepoint the code detects it but DON'T delete it! To allow deletion add `--env SP_DELETE=verify` to the command. This is to ensure that no file is deleted when you don't want that - or when the pull has problems.
+
--- index.js
+++ index.js
@@ -5,7 +5,8 @@
     SP_BASE_URL,
     SP_CRED_USER,
     SP_CRED_PASS,
-    SP_ROOT_FOLDER
+    SP_ROOT_FOLDER,
+    SP_DELETE
   } = process.env
 
   if ([SP_BASE_URL,
@@ -15,7 +16,7 @@
     throw 'You need to provide all required ENV-Vars'
   }
 
-  await sppull({
+  const result = await sppull({
     siteUrl: SP_BASE_URL,
     creds: {
       username: SP_CRED_USER,
@@ -27,4 +28,44 @@
   })
 
   console.log('PULL - Successfull')
+
+  const filesWritten = result.map(v=>v.SavedToLocalPath).sort()
+  const filesRead = readDirRec('/sharepoint')
+
+  const toDelete = filesRead.filter(v=>!filesWritten.includes(v))
+
+  if(toDelete.length===0) return;
+ 
+  console.log('exists-check | Finished')
+  console.log('files to delete')
+  console.log(toDelete.join('\n'))
+
+  if(SP_DELETE === 'verify') {
+    console.log('start deleting')
+    toDelete.map(f => {
+      unlinkSync(f)
+    })
+  } else {
+    console.log('To Delete these files set env SP_DELETE = "verify"')
+  }
 })()
+
+const { readdirSync, statSync, unlinkSync } = require('fs')
+const { join } = require('path')
+
+function readDirRec(dir) {
+  let r = []
+
+  const ff = readdirSync(dir)
+
+  ff.forEach(f => {
+    const file = join(dir, f)
+    if (statSync(file).isFile()) {
+      r.push(file)
+    } else {
+      r.push(...readDirRec(file))
+    }
+  })
+
+  return r
+}
